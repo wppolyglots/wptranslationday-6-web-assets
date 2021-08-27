698afe1225c97b29709c03a23d810db8a5c4b218
Boy Witthaya <ibdz.me@gmail.com>
2021-08-27 22:18:11 +0700

    Init child theme

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// enqueue parent styles
+add_action( 'wp_enqueue_scripts', 'wptd_enqueue_styles' );
+function wptd_enqueue_styles() {
+    $parenthandle = 'twenty-twenty-one-style'; // This is 'twenty-twenty-one-style' for the Twenty Twenty-one theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'custom-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
