a070dc66073e46f20a176c1a99808c0b040b7a43
Boy Witthaya <ibdz.me@gmail.com>
2021-09-02 02:04:10 +0700

    Update style for landing page title

--- functions.php
+++ functions.php
@@ -5,7 +5,8 @@ add_action( 'wp_enqueue_scripts', 'wptd_enqueue_styles' );
 function wptd_enqueue_styles() {
     $parenthandle = 'twenty-twenty-one-style'; // This is 'twenty-twenty-one-style' for the Twenty Twenty-one theme.
     $theme = wp_get_theme();
-    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+	wp_enqueue_style( 'add_google_fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;0,800;1,400&display=swap', false );
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css',
         array(),  // if the parent theme code has a dependency, copy it to here
         $theme->parent()->get('Version')
     );
